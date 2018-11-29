0627c28e234567fa22f8fb7e6ec3cfd751d97490
esteb <stevjocen@gmail.com>
2018-11-30 05:51:29 +0800

    Use datetime to use dates on the time series graph.

--- streaming/bitcoin_stream.py
+++ streaming/bitcoin_stream.py
@@ -2,6 +2,9 @@ from bokeh.io import curdoc
 from bokeh.models import ColumnDataSource, DatetimeTickFormatter
 from bokeh.plotting import figure
 from bs4 import BeautifulSoup
+from datetime import datetime
+from math import radians
+from pytz import timezone
 from random import randrange
 
 import requests
@@ -17,14 +20,19 @@ def extract_value():
     value_net = float(value_raw[0].span.text)
     return value_net
 
+#update data in graph
+def update():
+    new_data=dict(
+        x = [datetime.now()],
+        y = [extract_value()])
+    source.stream(new_data,rollover=15)
+    print(source.data)
+
 #create figure
-f = figure()
+f = figure(x_axis_type='datetime')
 
 #create data source
-source = ColumnDataSource(
-    dict(
-        x = [1], 
-        y = [extract_value()]))
+source = ColumnDataSource(dict(x = [], y = []))
 
 #create glyphs
 f.circle(
@@ -39,12 +47,18 @@ f.line(
     y = 'y',
     source = source)
 
-def update():
-    new_data=dict(
-        x = [source.data['x'][-1]+1],
-        y = [extract_value()])
-    source.stream(new_data,rollover=15)
-    print(source.data)
+f.xaxis.formatter = DatetimeTickFormatter(
+    seconds=["%Y-%m-%d-%H-%m-%S"],
+    minsec=["%Y-%m-%d-%H-%m-%S"],
+    minutes=["%Y-%m-%d-%H-%m-%S"],
+    hourmin=["%Y-%m-%d-%H-%m-%S"],
+    hours=["%Y-%m-%d-%H-%m-%S"],
+    days=["%Y-%m-%d-%H-%m-%S"],
+    months=["%Y-%m-%d-%H-%m-%S"],
+    years=["%Y-%m-%d-%H-%m-%S"],
+)
+
+f.xaxis.major_label_orientation=radians(90)
 
 curdoc().add_root(f)
 curdoc().add_periodic_callback(update, 2000)
