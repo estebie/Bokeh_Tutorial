47954f7629768a84ec4a599c566990514a8f91e1
esteb <stevjocen@gmail.com>
2018-11-29 20:59:19 +0800

    Plot bitcoin data.

--- streaming/bitcoin_stream.py
+++ streaming/bitcoin_stream.py
@@ -6,11 +6,25 @@ from random import randrange
 
 import requests
 
+#create webscraping function
+def extract_value():
+    r = requests.get(
+        "https://bitcoincharts.com/markets/coinbaseUSD.html", 
+        headers = {'User-Agent' : 'Mozilla/5.0'})
+    c = r.content
+    soup = BeautifulSoup(c, "html.parser")
+    value_raw = soup.find_all("p")
+    value_net = float(value_raw[0].span.text)
+    return value_net
+
 #create figure
-f = figure(x_range = (0,11), y_range = (0,11))
+f = figure()
 
 #create data source
-source = ColumnDataSource(dict(x = [], y = []))
+source = ColumnDataSource(
+    dict(
+        x = [1], 
+        y = [extract_value()]))
 
 #create glyphs
 f.circle(
@@ -20,19 +34,17 @@ f.circle(
     line_color = 'brown',
     source = source)
 
-#create webscraping function
-def extract_value():
-    r = requests.get("https://bitcoincharts.com/markets/coinbaseUSD.html", headers = {'User-Agent' : 'Mozilla/5.0'})
-    c = r.content
-    soup = BeautifulSoup(c, "html.parser")
-    value_raw = soup.find_all("p")
-    value_net = float(value_raw[0].span.text)
-    return value_net
+f.line(
+    x = 'x',
+    y = 'y',
+    source = source)
 
 def update():
-    new_data=dict(x=[randrange(1,10)],y=[randrange(1,10)])
+    new_data=dict(
+        x = [source.data['x'][-1]+1],
+        y = [extract_value()])
     source.stream(new_data,rollover=15)
     print(source.data)
 
 curdoc().add_root(f)
-curdoc().add_periodic_callback(update, 1000)
+curdoc().add_periodic_callback(update, 2000)
