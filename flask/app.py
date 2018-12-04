bd919259ff7332b5ce02acbe072228ecac2832d6
Steven Joseph Ceniza <ceniza.sjs@sprobe.com>
2018-12-04 15:40:17 +0800

    Embed a static graph in index.html.

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,14 @@
 #flask app
 
 from flask import Flask, render_template
-
+from BasicLine import js, div, cdn_css, cdn_js
 #instantiate the flask app
 app = Flask(__name__)
 
 #create index page function
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", js=js, div=div, cdn_css=cdn_css, cdn_js=cdn_js)
 
 #run the app
 if __name__ == "__main__":
