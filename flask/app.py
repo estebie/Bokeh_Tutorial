a14905502e74e3d89c1631ab917bcb86cc6870f1
Steven Joseph Ceniza <ceniza.sjs@sprobe.com>
2018-12-04 15:54:51 +0800

    Embed interactive plot in flask.

--- flask/app.py
+++ flask/app.py
@@ -2,13 +2,17 @@
 
 from flask import Flask, render_template
 from BasicLine import js, div, cdn_css, cdn_js
+from bokeh.embed import server_session
+from bokeh.util import session_id
+
 #instantiate the flask app
 app = Flask(__name__)
 
 #create index page function
 @app.route("/")
 def index():
-    return render_template("index.html", js=js, div=div, cdn_css=cdn_css, cdn_js=cdn_js)
+    server_script = server_session(None, session_id=session_id.generate_session_id(), url="http://localhost:5006/slider")
+    return render_template("index.html", graphname="Sliders", server_script=server_script)
 
 #run the app
 if __name__ == "__main__":
